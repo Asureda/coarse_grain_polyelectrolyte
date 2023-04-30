2482b7cd69e87d4c5cd8958757215ae6ce1b04e6
Alexandre Sureda Croguennoc <61150333+Asureda@users.noreply.github.com>
2023-05-01 01:13:34 +0200

    Update EnsembleDynamics.py

--- modules/EnsembleDynamics.py
+++ modules/EnsembleDynamics.py
@@ -1,3 +1,76 @@
+import numpy as np
+import pint
+import espressomd
+import pandas as pd
+import time
+espressomd.assert_features(["WCA", "ELECTROSTATICS"])
+import espressomd.electrostatics
+import espressomd.reaction_methods
+import espressomd.polymer
+
+from espressomd.interactions import HarmonicBond
+from espressomd.interactions import FeneBond
+from espressomd.interactions import AngleHarmonic
+from espressomd.interactions import Dihedral
+from Model import Model
+
+
+class EnsembleDynamics(Model):
+    def __init__(self, input_dict):
+        super().__init__(input_dict)
+        if self.USE_WCA:
+            self._warm_up_system()
+
+    def _warm_up_system(self):
+        print("Warm up the system")
+
+        warm_steps = 25
+        warm_n_times = 100
+        min_dist = 0.8
+        self.system.time = 0.
+        wca_cap = 5
+        self.system.force_cap = wca_cap
+        i = 0
+        act_min_dist = self.system.analysis.min_dist()
+        self.system.thermostat.set_langevin(kT=0.0, gamma=1.0, seed=4)
+        e_kin = np.zeros(warm_steps + wca_cap)
+
+        # warmup with zero temperature to remove overlaps
+        while act_min_dist < min_dist:
+            for j in range(warm_steps + wca_cap):
+                self.system.integrator.run(int(0.5 * warm_steps))
+                energy = self.system.analysis.energy()
+            #    system.integrator.run(warm_steps + wca_cap)
+            # Warmup criterion
+            act_min_dist = self.system.analysis.min_dist()
+            i += 1
+            wca_cap = wca_cap + 1
+            self.system.force_cap = wca_cap
+        print("Final min distance warm up", act_min_dist)
+        wca_cap = 0
+        self.system.force_cap = wca_cap
+        self.system.integrator.run(warm_steps)
+
+        # ramp up to simulation temperature
+        temp = 3.0
+        while temp > 1.0:
+            self.system.thermostat.set_langevin(kT=temp, gamma=1.0)
+            self.system.integrator.run(warm_steps)
+            temp -= 0.01
+        print("Final temperature warm up", temp)
+        self.system.thermostat.set_langevin(
+            kT=self.KT.to("sim_energy").magnitude, gamma=1.0, seed=1434
+        )
+        self.system.integrator.run(warm_steps)
+        print("Warm up finished")
+
+    def equilibrate_pH(self):
+
+        self.reaction1.reaction(reaction_steps= (self.n_nh) + 1)
+        self.reaction2.reaction(reaction_steps= (self.n_nh2) + 1)
+        if self.USE_WCA:
+            self.system.integrator.run(steps=1000)
+
     def perform_sampling(self,npH):
         
         self.system.time = 0.
